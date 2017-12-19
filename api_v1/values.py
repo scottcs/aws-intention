fa9beea9f13d07a1e7cb8a394bef981f7ee05a7d
scott <dev@cracklebones.com>
2017-12-19 11:08:05 -0800

    Add db to values api

--- api_v1/values.py
+++ api_v1/values.py
@@ -1,9 +1,16 @@
 """REST Values API."""
+import os
+
 from .rest import RestAPI
+from .db import DBClient
 
 
 class ValuesAPI(RestAPI):
     """Values API."""
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.db = DBClient(os.environ['USERS_TABLE'], id_string='email', debug=self.debug)
 
     def _get(self):
         return self._respond('GET invoked - Values')
