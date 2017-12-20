07a7eac40d35c363220b3b773fbc88d1b7bacaac
scott <dev@cracklebones.com>
2017-12-19 16:25:00 -0800

    Implement roles create/delete/get all

--- api_v1/roles.py
+++ api_v1/roles.py
@@ -14,9 +14,57 @@ class RolesAPI(RestAPI):
         self.db = DBClient(os.environ['USERS_TABLE'], id_string='email', debug=self.debug)
 
     def _post(self):
-        if not isinstance(self.body, list):
-            return self._respond(message='Body must be a list.', status=400)
-        response = self.db.update(self.db.current_user(), {'roles': unique_list(self.body)})
+        try:
+            name = self.body['name']
+        except (KeyError, TypeError):
+            return self._respond(message='"name" expected in body', status=400)
+        try:
+            values = self.body['values']
+            if not isinstance(values, list):
+                return self._respond(message='"values" must be a list.', status=400)
+        except (KeyError, TypeError):
+            return self._respond(message='"values" expected in body', status=400)
+        try:
+            aliases = self.body['aliases']
+            if not isinstance(aliases, list):
+                return self._respond(message='"aliases" must be a list.', status=400)
+        except (KeyError, TypeError):
+            aliases = []
+        response = self.db.get(self.db.current_user())
+        try:
+            roles = response.response['Item']['roles']
+        except (KeyError, TypeError):
+            roles = []
+        if name in [r['name'] for r in roles]:
+            return self._respond(message='Resource already exists', status=400)
+        try:
+            defined_values = response.response['Item']['values']
+        except (KeyError, TypeError):
+            defined_values = []
+        for value in values:
+            if value not in [v['name'] for v in defined_values]:
+                return self._respond(message=f'Undefined value "{value}"', status=400)
+        roles.append({
+            'name': name,
+            'values': values,
+            'aliases': unique_list(aliases)
+        })
+        response = self.db.update(self.db.current_user(), {'roles': roles})
+        return self._respond(message=response.message, status=response.status)
+
+    def _delete(self):
+        response = self.db.get(self.db.current_user())
+        try:
+            roles = response.response['Item']['roles']
+        except (KeyError, TypeError):
+            return self._respond(message='Not Found', status=404)
+        if self.path_parameters['name'] not in [r['name'] for r in roles]:
+            return self._respond(message='Not Found', status=404)
+        new_roles = []
+        for role in roles:
+            if self.path_parameters['name'] != role['name']:
+                new_roles.append(role)
+        response = self.db.update(self.db.current_user(), {'roles': new_roles})
         return self._respond(message=response.message, status=response.status)
 
     def _get(self):
--- routes.py
+++ routes.py
@@ -1,6 +1,7 @@
 """Handler for base API route."""
 from api_v1.users import UsersAPI
 from api_v1.values import ValuesAPI
+from api_v1.roles import RolesAPI
 
 
 def users(event, context):
@@ -21,3 +22,13 @@ def values(event, context):
     :return: Response dict
     """
     return ValuesAPI(event, context).invoke()
+
+
+def roles(event, context):
+    """Roles API handler.
+
+    :param event: Lambda event
+    :param context: Lambda context
+    :return: Response dict
+    """
+    return RolesAPI(event, context).invoke()
