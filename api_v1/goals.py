c0d89003f7e848c6779ea11bb83d19a7966c92f9
scott <dev@cracklebones.com>
2017-12-20 10:43:05 -0800

    Implement goal post

--- api_v1/goals.py
+++ api_v1/goals.py
@@ -1,7 +1,8 @@
 """REST Goals API."""
 import os
+import uuid
 
-from .rest import RestAPI
+from .rest import RestAPI, ValidationException
 from .db import DBClient
 
 
@@ -11,12 +12,48 @@ class GoalsAPI(RestAPI):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.db = DBClient(os.environ['GOALS_TABLE'], debug=self.debug)
+        self.user_db = DBClient(os.environ['USERS_TABLE'], debug=self.debug)
+
+    def _validate_goal(self, goal, update=False):
+        if not isinstance(goal, dict):
+            raise ValidationException('Goal is expected to be a dict')
+        if 'name' not in goal and not update:
+            raise ValidationException('"name" expected in body')
+        if 'role' not in goal and not update:
+            raise ValidationException('"role" expected in body')
+        if 'role' in goal:
+            role = goal['role']
+            if not self._is_role_defined(role):
+                raise ValidationException(f'Undefined role "{role}"')
+
+    def _is_role_defined(self, role):
+        response = self.user_db.get(self.user_db.current_user())
+        try:
+            defined_roles = response.response['Item']['roles']
+        except (KeyError, TypeError):
+            defined_roles = []
+        return role in [r['name'] for r in defined_roles]
+
+    def _make_body(self, update=False):
+        body = {
+            'user': self.db.current_user(),
+            'name': self.body['name'],
+            'role': self.body['role'],
+        }
+        for key in ('notes', 'parent', 'due', 'repeat'):
+            if key in self.body:
+                body[key] = self.body[key]
+        if update:
+            for key in ('complete',):
+                if key in self.body:
+                    body[key] = self.body[key]
+        return body
 
     def _post(self):
         try:
-            goal_id = self.body['id']
-        except KeyError:
-            return self._respond('Body must contain "id".', status=400)
-        body = {}
-        response = self.db.create(goal_id, body)
+            self._validate_goal(self.body)
+        except ValidationException as exc:
+            return self._respond(message=str(exc), status=400)
+        goal_id = str(uuid.uuid4())
+        response = self.db.create(goal_id, self._make_body())
         return self._respond(response.message, status=response.status)
